20d58bd9b5ea74f5a289c5938dbe377c05ba2b8a
Polina Kocheva <p.kocheva@gmail.com>
2020-12-28 00:28:43 +0100

    Remove unused imports from map

--- src/components/home/HomeMap.js
+++ src/components/home/HomeMap.js
@@ -2,10 +2,8 @@ import React, { Component } from "react"
 import { Link } from 'gatsby'
 import {
   ComposableMap,
-  ZoomableGroup,
   Geographies,
   Geography,
-  Markers,
   Marker,
 } from "react-simple-maps"
 
