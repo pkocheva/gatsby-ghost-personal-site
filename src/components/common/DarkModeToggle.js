27e5d1a0c673a8c39c8bd1bd9bbbf3699ad0c503
Polina Kocheva <p.kocheva@gmail.com>
2021-01-02 11:11:31 +0100

    Add dark mode

---
+++ src/components/common/DarkModeToggle.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react"
+
+class DarkModeToggle extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.handleChange = this.handleChange.bind(this);
+
+        this.state = {
+            darkModeOn: false
+        };
+    }
+
+    handleChange() {
+        if (!this.state.darkModeOn) document.body.classList.add('dark');
+        else document.body.classList.remove('dark');
+
+        this.state = {
+            darkModeOn: !this.state.darkModeOn
+        };
+
+        document.getElementById("darkModeToggle").checked = this.state.darkModeOn;
+        window.localStorage.setItem('darkmode', JSON.stringify(this.state.darkModeOn));
+    }
+
+    componentDidMount() {
+
+        const localStorageDarkMode = window.localStorage.getItem('darkmode');
+
+        if (localStorageDarkMode) {
+
+            if (localStorageDarkMode === 'true') {
+                document.body.classList.add('dark');
+            }
+
+            this.setState({
+                darkModeOn: localStorageDarkMode === 'true' ? true : false
+            }, () => {
+                document.getElementById("darkModeToggle").checked = this.state.darkModeOn
+            });
+        }
+
+    }
+
+    render() {
+        return (
+            <div className="darkModeToggle">
+                <input id="darkModeToggle" type="checkbox" onClick={this.handleChange}/>
+                <label for="darkModeToggle" className="check-trail">
+                    <span className="check-handler"></span>
+                </label>
+            </div>
+        )
+    }
+}
+
+export default DarkModeToggle
