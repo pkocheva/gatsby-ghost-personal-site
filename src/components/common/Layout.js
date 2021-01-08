93fe9e13d1b0586d86bf2cf0b172c1d7870d99dc
Polina Kocheva <p.kocheva@gmail.com>
2021-01-08 01:13:36 +0100

    Change Dev link in menu

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -28,7 +28,7 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
     const twitterUrl = site.twitter ? `https://twitter.com/${site.twitter.replace(/^@/, ``)}` : null
     const facebookUrl = site.facebook ? `https://www.facebook.com/${site.facebook.replace(/^\//, ``)}` : null
 
-    const menuLinks = <div><Link to="/tag/travel">🎒Travel</Link><Link to="/tag/dev">👩‍💻Code</Link><Link to="/tag/dev">✨More</Link></div>;
+    const menuLinks = <div><Link to="/tag/travel">🎒Travel</Link><Link to="/dev">👩‍💻Code</Link><Link to="/tag/dev">✨More</Link></div>;
 
     // maybe change this SVG for another wave
     const swirl = <svg preserveAspectRatio="none" width="1440" height="74" viewBox="0 0 1440 74" className="header-wave"><path d="M456.464 0.0433865C277.158 -1.70575 0 50.0141 0 50.0141V74H1440V50.0141C1440 50.0141 1320.4 31.1925 1243.09 27.0276C1099.33 19.2816 1019.08 53.1981 875.138 50.0141C710.527 46.3727 621.108 1.64949 456.464 0.0433865Z"></path></svg>
