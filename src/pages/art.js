87c0b47f3355488850ab2692d7aae59862b622f1
Polina Kocheva <p.kocheva@gmail.com>
2021-01-11 00:13:39 +0100

    Add art and temporarily disable instagram feed

--- src/pages/art.js
+++ src/pages/art.js
@@ -8,9 +8,13 @@ import './art.scss'
 
 const ArtPage = () => (
     <Layout>
-        <div className="container">
-            <p>See more on <a href="https://www.instagram.com/pbk_art/" target="_blank">Instagram</a>.</p>
-            <Feed userName="pbk_art" className="Feed" classNameLoading="Loading" /> 
+        <div className="container art">
+            <h1>Digital Paintings</h1>
+            <img className="art-image" src="/images/calm.jpg" />
+            <img className="art-image" src="/images/flower-shop.jpg" />
+            <img className="art-image" src="/images/willow-tree-island-house.jpg" />
+            {/*<Feed userName="pbk_art" className="Feed" classNameLoading="Loading" /> */}
+            <p className="art-see-more">See more on <a href="https://www.instagram.com/pbk_art/" target="_blank">Instagram</a>.</p>
         </div>
     </Layout>
 )
